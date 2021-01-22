405f8cc9e633a05f55f59ef5cc8b04a5533c1c45
Ander Swift <anderswift@gmail.com>
2021-01-22 00:33:50 -0500

    remove error message relying on  err object

--- backend/errors/errorHandler.js
+++ backend/errors/errorHandler.js
@@ -1,12 +1,8 @@
 module.exports = (err, req, res, next) => {
   let { statusCode = 500, message = 'An error occurred on the server' } = err;
 
-  if (err.name === 'ValidationError') {
-    statusCode = 400;
-  } else if (err.name === 'MongoError' && err.code === 11000) {
-    statusCode = 409;
-    message = `An account with the email address '${err.keyValue.email}' already exists.`;
-  }
+  if (err.name === 'ValidationError') statusCode = 400;
+  else if (err.name === 'MongoError' && err.code === 11000) statusCode = 409;
 
   res.status(statusCode).send({ message });
 };
